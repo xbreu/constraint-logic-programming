6269dff3e0e3bde4e08387c603431d7778419380
Alexandre Abreu <xbreu@tuta.io>
2022-03-29 16:53:12 +0100

    update: flags for labeling and fix number of repetitions

--- exercises/23-parity-sequences.pl
+++ exercises/23-parity-sequences.pl
@@ -1,6 +1,15 @@
 :- use_module(library(clpfd)).
 :- use_module(library(lists)).
 
+% Generates a sequence of five numbers, that can take any value between 1 and
+% 9, with the following constraints:
+% - There can be no two even or odd numbers in sequence;
+% - The number in the center of the sequence must be 1 or 2;
+% - The list cannot have repeated values.
+%
+% This can be seen in the following example:
+% ?- seq(Vars).
+% Vars = [3,2,1,4,5] ?
 seq(Vars) :-
     Vars = [A, B, C, D, E],
     domain(Vars, 1, 9),
@@ -11,15 +20,27 @@ seq(Vars) :-
     C mod 2 #= Parity1,
     D mod 2 #= Parity2,
     E mod 2 #= Parity1,
-    labeling([],Vars).
+    labeling([ff],Vars).
 
+% This is a variation of seq, but the list has a length of N, with the
+% following restrictions:
+% - N needs to be an odd number that is multiple of three;
+% - Each number can take a value between 1 and 9;
+% - A number can only appear, at maximum, three times in the list;
+% - There can be no two even or odd numbers in sequence;
+% - The number in the middle of the list must be greater than the first and
+%   last elements of it.
+%
+% This can be seen in the following example:
+% ?- seqn(Vars, 9).
+% Vars = [1,2,9,4,9,4,9,6,3] ?
 seqn(Vars, N) :-
     length(Vars, N),
     domain(Vars, 1, 9),
     limit_repetitions(Vars),
     limit_parity(Vars),
     limit_middle_number(Vars),
-    labeling([],Vars).
+    labeling([ff],Vars).
 
 limit_repetitions(Vars) :-
     limit_repetitions(Vars, Vars).
@@ -27,7 +48,7 @@ limit_repetitions(Vars) :-
 limit_repetitions([], _) :- !.
 limit_repetitions([X | Xs], List) :-
     exactly(X, List, N),
-    N #< 3,
+    N #<= 3,
     limit_repetitions(Xs, List).
 
 limit_parity(Vars) :-
